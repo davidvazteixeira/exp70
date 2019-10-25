68d16002dace0ce3238631af9afe8c6427a04166
David <dvt4qb@gmail.com>
2019-10-25 13:20:13 -0300

    updated git ignore; run.sh script at thermohygrometer;

--- thermohygrometer/python_backend/run.sh
+++ thermohygrometer/python_backend/run.sh
@@ -1,4 +1,64 @@
 #!/bin/sh
-xset -dpms s off
-cd /home/pi/Downloads/exp70/thermohygrometer/python_backend
-python3 backend.py &
+# Configure you actions here
+
+# ---------------------------
+# Before testing exp/test mode
+pre_run(){
+  xset -dpms s off
+  #cd /home/pi/Downloads/exp70/thermohygrometer/python_backend
+  source .env/bin/activate
+  python3 backend.py
+  source .env/bin/deactivate
+}
+
+# ---------------------------
+# Run if exp mode
+exp_run(){
+  echo "."
+}
+
+# Run if test mode
+test_run(){
+  echo "."
+}
+
+# ---------------------------
+# Run after exp mode
+after_exp_run(){
+  echo "."
+}
+
+# Run after test mode
+after_test_run(){
+  echo "."
+}
+
+# ---------------------------
+# Run at the end
+at_end(){
+  echo "."
+}
+
+if [ "$1" = '-h' ]; then
+  echo '  Script to simple boot project folder.'
+  echo
+  echo '  ./run.sh'
+  echo '    starts in test mode. Closes the ENV after exit.'
+  echo
+  echo '  ./run.sh'
+  echo '    starts in exp mode.'
+  exit
+fi
+
+pre_run
+if [ "$1" = "exp" ]; then
+  echo "exp mode"
+  exp_run
+  after_exp_run
+else
+  echo "test mode"
+  test_run
+  after_test_run
+fi
+at_end
+
---
+++ thermohygrometer/thermohygrometer.ino
@@ -0,0 +1,77 @@
+#include <SimpleDHT.h>
+
+#define PIN1 49
+#define VCC1 51
+#define GND1 53
+
+#define PIN2 33
+#define VCC2 35
+#define GND2 37
+
+SimpleDHT11 ht;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(GND1, OUTPUT);
+  pinMode(GND2, OUTPUT);
+  digitalWrite(GND1, LOW);
+  digitalWrite(GND2, LOW);
+  
+  pinMode(VCC1, OUTPUT);
+  pinMode(VCC2, OUTPUT);
+  digitalWrite(VCC1, HIGH);
+  digitalWrite(VCC2, HIGH);
+}
+
+byte tReturn, hReturn;
+byte getData(int pin){
+  byte t = 0;
+  byte h = 0;
+
+  int n=0;
+  
+  for(int i=0; i < 10; i++){
+    n++;
+    ht.read(pin, &t, &h, NULL);
+    if(t==0 && h==0){
+      delay(1000);
+    } else {
+      break;
+    }
+  }
+
+  tReturn = t;
+  hReturn = h;
+}
+
+void serialEvent(){
+  while(Serial.available()) Serial.read();
+
+  digitalWrite(13, HIGH);
+  if(c == 'c') sendData();
+  delay(500);
+  digitalWrite(13, LOW);
+  while(Serial.available()) Serial.read();
+}
+
+void sendData(){
+  byte t1, h1, t2, h2;   
+    getData(PIN1);
+    t1 = tReturn;
+    h1 = hReturn;
+    
+    getData(PIN2);
+    t2 = tReturn;
+    h2 = hReturn;
+  
+    Serial.print(t1);Serial.print(' ');
+    Serial.print(h1);Serial.print(' ');
+    Serial.print(t2);Serial.print(' ');
+    Serial.print(h2);Serial.println();
+}
+
+
+void loop() {
+  //sendData();
+}
